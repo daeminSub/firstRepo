3a28bdebb1644d7a52966aa5973268dfbe09fb24
daeminSub <dkseoals0216@naver.com>
2025-02-03 19:14:17 +0900

    badge JSX
    Co-authored-by: Cream_nayeon <eoals0216@gmail.com>

---
+++ first.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ function first() {
+    return (
+        <div className='badge'>
+            <h1>
+                hi! this JSX file was made to get a badge!
+            </h1>
+        </div>
+    );
+}
+
+export default first;
